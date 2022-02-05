d251b3792cc10856de418056df1364942ffa50e9
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-05 18:54:53 +0300

    doc;empty-div;i have add an empty div with class empty which will help me to style the submission page

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 function myButton(){
     var score=0
+   
+   //if(document.getElementById('correct1').checked)
+    //{
+    //score++;
+   //}
 
-   if(document.getElementById('correct1').checked)
-    {
-     score+=10;
-    }
+     // if(document.getElementById('correct2').checked)
+   // {
+   //   score++;
+    //}
 
-    if(document.getElementById('correct2').checked)
+   //if(document.getElementById('correct3').checked)
+    //{
+       //   score++;
+   // }
+    //var empty=document.getElementById("empty")
     {
-     score+=10;
-    }
+    //  score++;
+     }
+    //quiz.style.display="none";
+    //empty.textContent=score;
+    
+   
 
-    if(document.getElementById('correct3').checked)
-    {
-     score+=10;
-    }
-      document.write("Your score is;" +score)
+   //var currentAnswer1=document .first .question1 .value;
+   //var currentAnswer2=document .first .question2 .value;
+   //var currentAnswer3=document .first .question3 .value;
+
+   //var empty=document.getElementById("empty")
+   
+   //if (currentAnswer1=="Javascript"){score++};
+   //if (currentAnswer2=="Loop"){score++};
+   //if (currentAnswer3=="Boolean"){score++};
+   //first.style.display="none";
+   //empty.textContent="score"
+
+
+   //document.write("Your score is;"+score)
+
+
 }
