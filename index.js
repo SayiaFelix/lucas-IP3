0366ab5c99d6ba8aab22364c75b78582e85ffd59
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-05 18:59:21 +0300

    scr;scripting-document;I have script my document so that marks can be displayed after submission

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
 function myButton(){
     var score=0
    
-   //if(document.getElementById('correct1').checked)
-    //{
-    //score++;
-   //}
-
-     // if(document.getElementById('correct2').checked)
-   // {
-   //   score++;
-    //}
-
-   //if(document.getElementById('correct3').checked)
-    //{
-       //   score++;
-   // }
-    //var empty=document.getElementById("empty")
-    {
-    //  score++;
-     }
-    //quiz.style.display="none";
-    //empty.textContent=score;
-    
    
-
-   //var currentAnswer1=document .first .question1 .value;
-   //var currentAnswer2=document .first .question2 .value;
-   //var currentAnswer3=document .first .question3 .value;
+   var currentAnswer1=document .first .question1 .value;
+   var currentAnswer2=document .first .question2 .value;
+   var currentAnswer3=document .first .question3 .value;
 
    //var empty=document.getElementById("empty")
    
-   //if (currentAnswer1=="Javascript"){score++};
-   //if (currentAnswer2=="Loop"){score++};
-   //if (currentAnswer3=="Boolean"){score++};
+   if (currentAnswer1=="Javascript"){score++};
+   if (currentAnswer2=="Loop"){score++};
+   if (currentAnswer3=="Boolean"){score++};
    //first.style.display="none";
    //empty.textContent="score"
 
 
-   //document.write("Your score is;"+score)
+   document.write("Your score is;"+score)
 
 
 }
