a39756aa48fa9789d6d52d3652add1bbd020c4f7
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-06 16:05:25 +0300

    script;project scripting-i have script my project

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 
 
 function myButton(){
+
     var score=0;
    
    var question1=document .first .question1 .value;
    var question2=document .first .question2 .value;
    var question3=document .first .question3 .value;
 
-   var result= document.getElementById("submitboard")
-   //var quiz = document.getElementById("quiz");
-   if (question1=="Javascript"){score++};
-   if (question2=="Loop"){score++};
-   if (question3=="Boolean"){score++};
-   result.textContent=score;
-   //quiz.style.display="none";
+   
+   var submitboard= document.getElementById('submitboard');
+   
 
-   // document.write (score)
 
-   //if (score==3) {
-  //  submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/3*100)+"%"+""
-  // } else if (score==2) {
-  //  submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/3*100)+"%"+""
-  // } else {
-  //   submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/3*100)+"%. Kindly Reattempt the quiz!!!"  
-  // }
+   if (question1=="Javascript"){score+=30};
+   if (question2=="Loop"){score+=30};
+   if (question3=="Boolean"){score+=30};
+
+
+   java.style.display="none";
+   submitboard.textContent=score;
+   //document.write(Math.floor(score/90*100)+"%");
+
+  
+
+  if (score==3){
+       submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/90*100)+"%"+""
+    } else if (score==2) {
+      submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/90*100)+"%"+""
+    } else {
+       submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/90*100)+"%. Kindly Reattempt the quiz!!!"  
+    }
+   
   }
-
-
-
-   //var messages=["Congratulations!! You Have Passed. Your score is " + Math.floor(score/3*100)+"%"+"",  "","Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/3*100)+"%. Kindly Reattempt the quiz!!!"];
-
-   //var range;
-   //      if(score>2){
-  //     range=0;
-  //     }
-   //      if(score=2){
-   //       range=1;
-  //     }
-   //     if(score<=1){
-   ////       range=2;
-     //  }
-
-
-   // 
-
-
-   //document.getElementById("messages").innerHTML=messages[range];
+  
