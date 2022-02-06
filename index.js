b72ab14a147f9e08535416192f015e06a86d33c5
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-06 13:32:15 +0300

    style;script-styling;i have call the submit div and script together with html file

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-//const text=document.querySelector(".quiz");
-//const strText=text.textContent;
-//console.log(strText)
+
 
 function myButton(){
     var score=0;
-   
    
    var question1=document .first .question1 .value;
    var question2=document .first .question2 .value;
    var question3=document .first .question3 .value;
 
-    
-
+   var result= document.getElementById("submitboard")
+   //var quiz = document.getElementById("quiz");
    if (question1=="Javascript"){score++};
    if (question2=="Loop"){score++};
    if (question3=="Boolean"){score++};
+   result.textContent=score;
+   //quiz.style.display="none";
+
+   // document.write (score)
+
+   //if (score==3) {
+  //  submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/3*100)+"%"+""
+  // } else if (score==2) {
+  //  submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/3*100)+"%"+""
+  // } else {
+  //   submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/3*100)+"%. Kindly Reattempt the quiz!!!"  
+  // }
+  }
 
 
 
+   //var messages=["Congratulations!! You Have Passed. Your score is " + Math.floor(score/3*100)+"%"+"",  "","Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/3*100)+"%. Kindly Reattempt the quiz!!!"];
 
-   var messages=["Congratulations!! You Have Passed. Your score is " + Math.floor(score/3*100)+"%"+"",  "Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/3*100)+"%"+"","Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/3*100)+"%. Kindly Reattempt the quiz!!!"];
-
-   var range;
-         if(score>2){
-       range=0;
-       }
-         if(score=2){
-          range=1;
-       }
-        if(score<=1){
-          range=2;
-       }
+   //var range;
+   //      if(score>2){
+  //     range=0;
+  //     }
+   //      if(score=2){
+   //       range=1;
+  //     }
+   //     if(score<=1){
+   ////       range=2;
+     //  }
 
 
-   document.getElementById("empty").style.display="none";
+   // 
 
-   document.getElementById("messages").innerHTML=messages[range];
-   document.getElementById("correct").innerHTML="You Score " + score + " correct";
-   
-     document.write (score)
- }
 
- 
+   //document.getElementById("messages").innerHTML=messages[range];
