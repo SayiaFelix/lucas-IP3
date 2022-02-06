1191f7d47fdbedd111d7c90a900ee2709dba2cd8
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-06 16:08:11 +0300

    scripting;project scripting- Ihave script the project and add math.floor

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ function myButton(){
 
    java.style.display="none";
    submitboard.textContent=score;
-   //document.write(Math.floor(score/90*100)+"%");
+   document.write(Math.floor(score/90*100)+"%");
 
   
 
-  if (score==3){
-       submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/90*100)+"%"+""
-    } else if (score==2) {
-      submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/90*100)+"%"+""
-    } else {
-       submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/90*100)+"%. Kindly Reattempt the quiz!!!"  
-    }
+  //if (score==3){
+    //   submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/90*100)+"%"+""
+   // } else if (score==2) {
+    //  submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/90*100)+"%"+""
+  //  } else {
+    //   submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/90*100)+"%. Kindly Reattempt the quiz!!!"  
+    //}
    
   }
   
