44fefd3011ce2602e26c370e4dc4f8d80ddacd2b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-06 07:39:36 +0300

    styling;submit div styling-i have style submit div

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 function myButton(){
-    var score=0
+    var score=0;
    
    
-   var currentAnswer1=document .first .question1 .value;
-   var currentAnswer2=document .first .question2 .value;
-   var currentAnswer3=document .first .question3 .value;
+   var question1=document .first .question1 .value;
+   var question2=document .first .question2 .value;
+   var question3=document .first .question3 .value;
 
-   //var empty=document.getElementById("empty")
+    
+
+   if (question1=="Javascript"){score++};
+   if (question2=="Loop"){score++};
+   if (question3=="Boolean"){score++};
+
+
+   document.getElementById("empty").style.display="none";
+   document.getElementById("correct").innerHTML="You Score " + score + " correct";
    
-   if (currentAnswer1=="Javascript"){score++};
-   if (currentAnswer2=="Loop"){score++};
-   if (currentAnswer3=="Boolean"){score++};
-   //first.style.display="none";
-   //empty.textContent="score"
+     
 
+     //document.write (score)
 
-   document.write("Your score is;"+score)
+    
 
-
-}
+  };
