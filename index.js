69225451cf1017af8e2797d45af38295aa71e70c
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-02-06 17:34:44 +0300

    docs;styling-doc; I have add some styling in my project

--- index.js
+++ index.js
@@ -2,37 +2,33 @@
 
 function myButton(){
 
-    var score=0;
-   
-   var question1=document .first .question1 .value;
-   var question2=document .first .question2 .value;
-   var question3=document .first .question3 .value;
+  var score=0;
+ 
+ var question1=document .first .question1 .value;
+ var question2=document .first .question2 .value;
+ var question3=document .first .question3 .value;
 
-   
+ var submitboard= document.getElementById('submitboard');
 
-   if (question1=="Javascript"){score+=30};
-   if (question2=="Loop"){score+=30};
-   if (question3=="Boolean"){score+=30};
+ if (question1=="Javascript"){score+=30};
+ if (question2=="Loop"){score+=30};
+ if (question3=="Boolean"){score+=30};
 
-   var submitboard= document.getElementById('submitboard');
-   myButton=function(){
+ submitboard.innerHTML=score;
+ java.style.display="none";
 
-  if (score==3){
-     submitboard.textContent="Congratulations!! You Have Passed. Your score is " + Math.floor(score/90*100)+"%"+""
-   }
-   else if (score==2) {
-     submitboard.textContent="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/90*100)+"%"+""
-   } 
-    else {
-     submitboard.textContent="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/90*100)+"%. Kindly Reattempt the quiz!!!"  
-  }
-   
-   }
 
-   java.style.display="none";
-   submitboard.textContent=score;
-   document.write(Math.floor(score/90*100)+"%");
+if (score==3){
+   submitboard.innerHTML="Congratulations!! You Have Passed. Your score is " + Math.floor(score/90*100) +"%"+".Excellent!!"
+ }
+ else if (score==2) {
+   submitboard.innerHTML="Congratulations!! You Have Fairly Passed. Your score is " + Math.floor(score/90*100) +"%"+".Average!!"
+ } 
+ else {
+  submitboard.innerHTML="Congratulations!! Unfortunately you have failed. Your score is " + Math.floor(score/90*100) +"%"+".Fail!!!"
+}
+document.write(Math.floor(score/90*100)+"%");
 
-  
-  }
-  
+
+};
+
